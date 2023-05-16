2e64fbf0b07e3b3064c1cfe91be8be9d5cb018b3
Szymon Rosztajn <131609082+SzymonRosztajn@users.noreply.github.com>
2023-05-16 18:45:58 +0200

    inserts commit

---
+++ inserts.sql
@@ -0,0 +1,181 @@
+
+
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (1, 'WAUSH78E78A079941', '2021-10-15', 'Red', 'Denmark', 196094, 1);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (2, '1N6AF0LX2FN546528', '2022-05-07', 'Purple', 'Croatia', 135982, 2);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (3, 'WA1MYAFE1AD633856', '2022-01-25', 'Turquoise', 'Brazil', 108411, 3);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (4, 'JM3TB2BV9E0902365', '2022-02-18', 'Purple', 'Philippines', 44320, 4);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (5, 'WVWAA7AJXBW488567', '2022-08-25', 'Red', 'Malaysia', 145004, 5);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (6, 'WBA3F9C50FK817616', '2022-01-23', 'Blue', 'Brazil', 58769, 6);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (7, '1C4RDHDGXDC296179', '2022-07-30', 'Yellow', 'Poland', 165986, 7);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (8, '1G6DW677X70417211', '2021-12-12', 'Teal', 'Russia', 48534, 8);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (9, '1G6DP577470237777', '2022-03-27', 'Violet', 'Hungary', 74124, 9);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (10, '5J8TB4H33DL114464', '2022-09-11', 'Orange', 'Sweden', 117198, 10);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (11, '1FMEU7EE0AU054487', '2023-01-10', 'Crimson', 'Chad', 119680, 11);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (12, 'WBA3T3C59F5583416', '2021-12-01', 'Mauv', 'Indonesia', 160208, 12);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (13, '1FTEW1CM3DF583933', '2021-09-30', 'Violet', 'Indonesia', 177114, 13);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (14, 'WAUGL78E77A234779', '2022-07-30', 'Goldenrod', 'Russia', 167854, 14);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (15, '1N6AA0CC1DN220647', '2021-09-07', 'Crimson', 'Indonesia', 37094, 15);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (16, 'JTDKDTB34F1098930', '2022-04-20', 'Aquamarine', 'China', 70936, 16);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (17, 'KNAFZ6A34F5539435', '2021-08-08', 'Orange', 'France', 133384, 17);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (18, '3C6LD4AT0CG925029', '2022-08-14', 'Turquoise', 'Japan', 133570, 18);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (19, 'WAUNF98P96A507506', '2022-04-01', 'Green', 'Indonesia', 153111, 19);
+insert into Pojazd (id, Vin, Data_prod, Kolor, Pochodzenie, Przebieg, Model_id) values (20, '3FAHP0DC5BR673085', '2022-03-28', 'Puce', 'China', 144674, 20);
+
+insert into Model (id, Nazwa, Wersja, Marka_id) values (1, 'Gallardo', 7, 1);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (2, 'A3', 7, 2);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (3, 'Range Rover', 4, 3);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (4, '914', 3, 4);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (5, 'Sienna', 3, 5);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (6, 'Quattroporte', 5, 6);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (7, 'Boxster', 6, 7);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (8, 'Grand Marquis', 6, 8);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (9, 'LTD Crown Victoria', 5, 9);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (10, 'Gallardo', 3, 10);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (11, 'Tacoma', 2, 11);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (12, 'Town Car', 6, 12);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (13, 'Journey', 5, 13);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (14, 'Mark LT', 1, 14);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (15, 'CTS', 1, 15);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (16, 'Legend', 1, 15);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (17, '300ZX', 3, 17);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (18, 'Forester', 7, 18);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (19, 'TrailBlazer', 7, 19);
+insert into Model (id, Nazwa, Wersja, Marka_id) values (20, 'Explorer', 1, 20);
+
+
+
+insert into Marka (id, Nazwa) values (1, 'Toyota');
+insert into Marka (id, Nazwa) values (2, 'Mazda');
+insert into Marka (id, Nazwa) values (3, 'Pontiac');
+insert into Marka (id, Nazwa) values (4, 'Saab');
+insert into Marka (id, Nazwa) values (5, 'Ford');
+insert into Marka (id, Nazwa) values (6, 'GMC');
+insert into Marka (id, Nazwa) values (7, 'Lexus');
+insert into Marka (id, Nazwa) values (8, 'Ford');
+insert into Marka (id, Nazwa) values (9, 'Toyota');
+insert into Marka (id, Nazwa) values (10, 'Ford');
+insert into Marka (id, Nazwa) values (11, 'Lotus');
+insert into Marka (id, Nazwa) values (12, 'Hyundai');
+insert into Marka (id, Nazwa) values (13, 'Kia');
+insert into Marka (id, Nazwa) values (14, 'Dodge');
+insert into Marka (id, Nazwa) values (15, 'Geo');
+insert into Marka (id, Nazwa) values (16, 'Cadillac');
+insert into Marka (id, Nazwa) values (17, 'Mercedes-Benz');
+insert into Marka (id, Nazwa) values (18, 'Mazda');
+insert into Marka (id, Nazwa) values (19, 'Mercedes-Benz');
+insert into Marka (id, Nazwa) values (20, 'Nissan');
+
+
+
+insert into Klient (id, Imie, Nazwisko, Telefon) values (1, 'Edi', 'Siggers', 505023967);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (2, 'Netta', 'Samett', 503779930);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (3, 'Vincent', 'Abele', 504595641);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (4, 'Seline', 'Bernot', 503582062);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (5, 'Sal', 'Khan', 503727614);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (6, 'Burnaby', 'Brind', 504796989);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (7, 'Hunfredo', 'Shinner', 505058152);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (8, 'Randie', 'Aleixo', 504103088);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (9, 'Lilith', 'Koppeck', 504079190);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (10, 'Grissel', 'Band', 503525797);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (11, 'Cheslie', 'Shanahan', 504574316);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (12, 'Amelita', 'Paddemore', 503299931);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (13, 'Sonia', 'Grayston', 504901222);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (14, 'Boy', 'Aughtie', 504543849);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (15, 'Brewster', 'Calow', 504656498);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (16, 'Twyla', 'Conrath', 503377852);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (17, 'Farah', 'Critzen', 504243669);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (18, 'Fabe', 'Leahair', 503578919);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (19, 'Culver', 'Mallall', 503424977);
+insert into Klient (id, Imie, Nazwisko, Telefon) values (20, 'Tammy', 'Fedder', 503814119);
+
+
+
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (1, 'ogledziny', '2018-05-30', 1, 1, 1);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (2, 'ogledziny', '2014-08-14', 2, 2, 2);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (3, 'ogledziny', '2022-03-30', 3, 3, 3);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (4, 'sprzedaz', '2017-01-08', 4, 4, 4);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (5, 'ogledziny', '2014-11-08', 5, 5, 5);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (6, 'ogledziny', '2000-03-29', 6, 6, 6);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (7, 'sprzedaz', '2010-11-22', 7, 7, 7);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (8, 'ogledziny', '2002-08-22', 8, 8, 8);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (9, 'ogledziny', '2017-06-26', 9, 9, 9);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (10, 'sprzedaz', '2007-07-04', 10, 10, 10);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (11, 'sprzedaz', '2010-12-30', 11, 11, 11);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (12, 'sprzedaz', '2016-04-09', 12, 12, 12);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (13, 'sprzedaz', '2011-07-13', 13, 13, 13);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (14, 'sprzedaz', '2013-07-14', 14, 14, 14);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (15, 'ogledziny', '2016-03-27', 15, 15, 15);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (16, 'ogledziny', '2016-08-23', 16, 16, 16);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (17, 'ogledziny', '2003-04-07', 17, 17, 17);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (18, 'ogledziny', '2007-11-25', 18, 18, 18);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (19, 'ogledziny', '2015-02-16', 19, 19, 19);
+insert into Wizyta (id, Nazwa, data_in, Pojazd_id, Pracownik_id, Klient_id) values (20, 'ogledziny', '2009-01-11', 20, 20, 20);
+
+
+
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (1, 'Lian', 'Rechert', 687530153, 'Sprzedawca', 4826, 1);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (2, 'Malia', 'Terram', 740536472, 'Sprzedawca', 3535, 2);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (3, 'Roosevelt', 'Matura', 684157100, 'Mechanik', 5159, 3);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (4, 'Slade', 'Jestico', 736520500, 'Sprzedawca', 7280, 4);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (5, 'Baxy', 'Heffernan', 755130656, 'Mechanik', 5984, 5);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (6, 'Karyl', 'Tweddle', 671392755, 'Mechanik', 4257, 6);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (7, 'Constanta', 'Gusticke', 726400922, 'Sprzedawca', 6174, 7);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (8, 'Cesaro', 'Trevains', 701269716, 'Mechanik', 5029, 8);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (9, 'De', 'Di Pietro', 776493952, 'Sprzedawca', 4752, 9);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (10, 'Devy', 'Frise', 760787101, 'Mechanik', 6952, 10);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (11, 'Sheeree', 'Danielsen', 679279280, 'Sprzedawca', 5110, 11);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (12, 'Flss', 'Gozzett', 759065597, 'Sprzedawca', 5219, 12);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (13, 'My', 'Grovier', 718761954, 'Mechanik', 4136, 13);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (14, 'Manolo', 'Gillino', 759545861, 'Mechanik', 6543, 14);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (15, 'Shanie', 'Bolver', 699280215, 'Sprzedawca', 6669, 15);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (16, 'Fletcher', 'Lowdwell', 690782457, 'Sprzedawca', 5737, 16);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (17, 'Timmie', 'Begbie', 742854342, 'Mechanik', 6197, 17);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (18, 'Port', 'Bote', 763024227, 'Mechanik', 4288, 18);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (19, 'Gratia', 'Port', 676025496, 'Mechanik', 6253, 19);
+insert into Pracownik (id, Imie, Nazwisko, nr_konta, Stanowisko, Pensja, Raport_id) values (20, 'Rod', 'Letts', 772574991, 'Mechanik', 5778, 20);
+
+
+
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (1, true, true, 104764, 'Aenean fermentum.', 1);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (2, false, true, 103881, 'Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 2);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (3, false, true, 143386, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.', 3);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (4, false, false, 130576, 'In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.', 4);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (5, true, true, 94355, 'Suspendisse ornare consequat lectus.', 5);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (6, true, true, 12661, 'Sed ante. Vivamus tortor. Duis mattis egestas metus.', 6);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (7, true, true, 77686, 'Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend.', 7);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (8, true, true, 76773, 'Morbi a ipsum. Integer a nibh. In quis justo.', 8);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (9, false, false, 104462, 'Integer ac neque.', 9);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (10, false, true, 60372, 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue.', 10);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (11, true, false, 32943, 'Aenean lectus.', 11);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (12, false, true, 127943, 'Morbi vel lectus in quam fringilla rhoncus.', 12);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (13, false, true, 34502, 'Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.', 13);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (14, true, true, 25854, 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 14);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (15, true, true, 34974, 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.', 15);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (16, false, false, 77619, 'In eleifend quam a odio. In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. ', 16);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (17, false, true, 13205, 'Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat.', 17);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (18, false, true, 117495, 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam. Nam tristique tortor eu pede.', 18);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (19, false, false, 14845, 'Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros', 19);
+insert into Raport (id, Sprawny, Czysty, Wartość, Opis, Pojazd_id) values (20, false, false, 123374, 'Duis bibendum.', 20);
+
+
+
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (1, '2012-07-09', '2011-07-14', 1, 1);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (2, '2004-05-06', '2021-10-12', 2, 2);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (3, '2006-03-25', '2010-03-23', 3, 3);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (4, '2018-08-11', '2001-05-25', 4, 4);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (5, '2014-10-02', '2015-06-09', 5, 5);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (6, '2022-10-01', '2014-07-30', 6, 6);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (7, '2002-11-29', '2014-12-29', 7, 7);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (8, '2013-10-15', '2006-08-23', 8, 8);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (9, '2009-12-11', '2021-11-04', 9, 9);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (10, '2006-01-31', '2017-02-03', 10, 10);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (11, '2013-09-03', '2009-03-02', 11, 11);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (12, '2010-06-29', '2013-02-24', 12, 12);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (13, '2003-02-24', '2002-06-02', 13, 13);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (14, '2002-04-21', '2015-09-04', 14, 14);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (15, '2004-08-17', '2020-06-29', 15, 15);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (16, '2000-06-10', '2001-09-24', 16, 16);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (17, '2007-04-08', '2021-04-26', 17, 17);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (18, '2001-11-15', '2007-09-16', 18, 18);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (19, '2006-07-26', '2013-07-09', 19, 19);
+insert into Rezerwacja (id, Start_rezerwacji, Koniec_rezerwacji, Pojazd_id, Klient_id) values (20, '2001-03-14', '2014-12-12', 20, 20);
